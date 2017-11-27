fcebd369d6a4cc8f0246c237af8177125fea5a7e
DarkyZ <s.soulie@hotmail.fr>
2017-11-27 23:45:55 +0100

    :ok_hand: Setup bases

---
+++ chip8/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/app.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'chip8.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    devServer: {
+        historyApiFallback: true,
+        noInfo: true,
+        overlay: true
+    },
+    performance: {
+        hints: false
+    },
+    devtool: 'inline-source-map'
+};
+
+if (process.env.NODE_ENV === 'production')
+{
+    module.exports.devtool = '#source-map';
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        })
+    ]);
+}
