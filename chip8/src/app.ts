35ef2eeeb195f1dc3f36b310a488dcb057f97d34
DarkyZ <s.soulie@hotmail.fr>
2017-11-28 01:21:01 +0100

    :ok_hand: Create canevas

--- chip8/src/app.ts
+++ chip8/src/app.ts
@@ -1,1 +1,73 @@
-console.log('Working !');
+const app:HTMLElement = document.getElementById('app');
+
+function createCanevas(): HTMLElement
+{
+    const canevas:HTMLElement = document.createElement('canevas');
+
+    /* Chip 8 resolution: 64×32 */
+    canevas.style.width = '640px';
+    canevas.style.height = '320px';
+    app.appendChild(canevas);
+    return canevas;
+}
+
+createCanevas();
+
+/*
+** Specs : http://devernay.free.fr/hacks/chip8/C8TECH10.HTM
+**
+** --------------------------------
+**          Chip8 Opcodes:
+** --------------------------------
+**
+** 00E0 	CLS
+** 00EE 	RET
+** 0NNN 	SYS addr
+** 1NNN 	JP addr
+** 2NNN 	CALL addr
+** 3XNN 	SE Vx, byte
+** 4XNN 	SNE Vx, byte
+** 5XY0 	SE Vx, Vy
+** 6XNN 	LD Vx, byte
+** 7XNN 	ADD Vx, byte
+** 8XY0 	LD Vx, Vy
+** 8XY1 	OR Vx, Vy
+** 8XY2 	AND Vx, Vy
+** 8XY3 	XOR Vx, Vy
+** 8XY4 	ADD Vx, Vy
+** 8XY5 	SUB Vx, Vy
+** 8XY6 	SHR Vx {, Vy}
+** 8XY7 	SUBN Vx, Vy
+** 8XYE 	SHL Vx {, Vy}
+** 9XY0 	SNE Vx, Vy
+** ANNN 	LD I, addr
+** BNNN 	JP V0, addr
+** CXNN 	RND Vx, byte
+** DXYN 	DRW Vx, Vy, nibble
+** EX9E 	SKP Vx
+** EXA1 	SKNP Vx
+** FX07 	LD Vx, DT
+** FX0A 	LD Vx, K
+** FX15 	LD DT, Vx
+** FX18 	LD ST, Vx
+** FX1E 	ADD I, Vx
+** FX29 	LD F, Vx
+** FX33 	LD B, Vx
+** FX55 	LD [I], Vx
+** FX65 	LD Vx, [I]
+**
+** --------------------------------
+**          Super Chip-48:
+** --------------------------------
+**
+** 00Cn     SCD nibble
+** 00FB     SCR
+** 00FC     SCL
+** 00FD     EXIT
+** 00FE     LOW
+** 00FF     HIGH
+** Dxy0     DRW Vx, Vy, 0
+** Fx30     LD HF, Vx
+** Fx75     LD R, Vx
+** Fx85     LD Vx, R
+*/
