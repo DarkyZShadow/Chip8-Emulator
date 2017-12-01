05c9241fe12ebbbdf5fa552c5aef2018bf0fb4fb
DarkyZ <s.soulie@hotmail.fr>
2017-12-01 20:27:37 +0100

    :ok_hand: Created 'CanvasManager' class

--- chip8/src/app.ts
+++ chip8/src/app.ts
@@ -1,23 +1,115 @@
 const app:HTMLElement = document.getElementById('app');
 
-function createCanvas(): HTMLCanvasElement
+interface ICanvasManagerOptions
 {
-    const canvas:HTMLCanvasElement = document.createElement('canvas');
-
-    /* Chip 8 resolution: 64×32 */
-    canvas.width = 64;
-    canvas.height = 32;
-    canvas.style.width = '640px';
-    canvas.style.height = '320px';
-    app.appendChild(canvas);
-    return canvas;
+    width: number;
+    height: number;
+    scale?: number;
+    canvasWidth?: number;
+    canvasHeight?: number;
+    backgroundColor?: string;
 }
 
-const canvas:HTMLCanvasElement = createCanvas();
-const ctx = canvas.getContext('2d');
+class CanvasManager
+{
+    /* Properties */
+    private readonly ctx:CanvasRenderingContext2D;
+    private readonly canvas:HTMLCanvasElement;
+    private readonly canvasWidth:number;
+    private readonly canvasHeight:number;
+    private readonly width:number;
+    private readonly height:number;
+    private readonly scale:number;
+    private isPathCreated:boolean;
 
-console.log(ctx);
+    constructor(options: ICanvasManagerOptions)
+    {
+        const { width, height, scale, canvasWidth, canvasHeight, backgroundColor } = options;
 
+        /* Some properties */
+        this.width = width;
+        this.height = height;
+        this.scale = scale || 10;
+        this.canvasWidth = canvasWidth || width * this.scale;
+        this.canvasHeight = canvasHeight || height * this.scale;
+        this.isPathCreated = false;
+
+        /* Create canvas */
+        this.canvas = document.createElement('canvas');
+        this.canvas.width = width * this.scale;
+        this.canvas.height = height * this.scale;
+        this.canvas.style.width = this.canvasWidth + 'px';
+        this.canvas.style.height = this.canvasHeight + 'px';
+        this.canvas.style.backgroundColor = backgroundColor || 'transparent';
+
+        /* Setup context */
+        this.ctx = this.canvas.getContext('2d');
+        this.ctx.lineWidth = this.scale;
+    }
+
+    public bind(parent: HTMLElement): void
+    {
+        parent.appendChild(this.canvas);
+    }
+
+    public test()
+    {
+        this.ctx.beginPath();
+
+        this.ctx.moveTo(0, this.scale / 2);
+        this.ctx.lineTo(this.width * this.scale, this.scale / 2);
+
+        this.ctx.moveTo(0, this.height * this.scale - this.scale / 2);
+        this.ctx.lineTo(this.width * this.scale, this.height * this.scale - this.scale / 2);
+
+        this.ctx.stroke();
+    }
+
+    public drawPoint(x: number, y: number)
+    {
+        this.initPath();
+        this.ctx.rect(x * this.scale + this.scale / 2, y * this.scale + this.scale / 2, this.scale, this.scale);
+        /*this.ctx.moveTo(x * this.scale + this.scale / 2, y * this.scale + this.scale / 2);
+        this.ctx.lineTo((x + 1) * this.scale + this.scale / 2, (y + 1) * this.scale + this.scale / 2);
+        console.log((x + 1) * this.scale + this.scale / 2, (y + 1) * this.scale + this.scale / 2);*/
+    }
+
+    public render(needToClosePath:boolean = false): boolean
+    {
+        if (!this.isPathCreated)
+            return false;
+
+        if (needToClosePath)
+            this.ctx.closePath();
+
+        this.ctx.stroke();
+        this.isPathCreated = false;
+    }
+
+    private initPath()
+    {
+        if (this.isPathCreated)
+            return;
+
+        this.ctx.beginPath();
+        this.isPathCreated = true;
+    }
+}
+
+const options = {
+    width: 64,
+    height: 32,
+    scale: 100,
+    canvasWidth: 640,
+    canvasHeight: 320,
+    backgroundColor: 'white'
+};
+const manager:CanvasManager = new CanvasManager(options);
+
+manager.bind(app);
+//manager.drawPoint(0, 0);
+manager.drawPoint(62, 30);
+manager.render();
 
 /*
 ** Specs : http://devernay.free.fr/hacks/chip8/C8TECH10.HTM
